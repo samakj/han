e11b88c7ea8156f7f8e4639173c32a178e507125
Sam Jones <samakj@live.co.uk>
2020-04-17 09:15:38 +0100

    Add client for an MQTT server

--- libs/han_mqtt/__init__.py
+++ libs/han_mqtt/__init__.py
@@ -1,1 +1,136 @@
+from typing import Any, Callable, Dict
+
+from paho.mqtt.client import Client as MQTTClient
+from paho.mqtt.client import MQTTMessageInfo
+
+from han_mqtt.handlers.ConnectionHandler import ConnectionHandler
+from han_mqtt.handlers.MessageHandler import MessageHandler
+from han_mqtt.handlers.PublishHandler import PublishHandler
+from han_mqtt.handlers.SubscriptionHandler import SubscriptionHandler
+from han_mqtt.models.UserData import MqttUserData
 from han_mqtt.topics.TopicBlueprint import TopicBlueprint
+from han_mqtt.topics.default_topics import DEFAULT_TOPIC_BLUEPRINT
+
+
+class HanMqttClient:
+    def __init__(
+        self, client_id: str, host: str, port: int, user_data: MqttUserData = None
+    ):
+        self.user_data = user_data
+        self.message_handler = MessageHandler()
+
+        self.client = MQTTClient(client_id=client_id, userdata=user_data)
+        self.connection_start = None
+
+        self.connection_handler = ConnectionHandler(
+            client=self.client, host=host, port=port
+        )
+        self.publish_handler = PublishHandler(client=self.client)
+        self.subscription_handler = SubscriptionHandler(client=self.client)
+
+        self.client.on_message = self.message_handler.handle_message
+        self.client.on_publish = self.publish_handler.handle_publish
+        self.client.on_subscribe = self.subscription_handler.handle_subscribe
+        self.client.on_unsubscribe = self.subscription_handler.handle_unsubscribe
+        self.client.on_connect = self.connection_handler.handle_connect
+        self.client.on_disconnect = self.connection_handler.handle_disconnect
+
+        self.add_topic_blueprint(DEFAULT_TOPIC_BLUEPRINT)
+
+    def subscribe(
+        self,
+        topic: str,
+        qos: int = 0,
+        options: Dict[str, Any] = None,
+        properties: Dict[str, Any] = None,
+    ) -> None:
+        if self.client.is_connected():
+            self.subscription_handler.subscribe(
+                topic=topic, qos=qos, options=options, properties=properties
+            )
+        else:
+            self.subscription_handler.lazy_subscribe(
+                topic=topic, qos=qos, options=options, properties=properties
+            )
+
+    def publish(
+        self,
+        topic: str,
+        payload: Dict[str, Any] = None,
+        qos: int = 0,
+        retain: bool = False,
+    ) -> MQTTMessageInfo:
+        return self.publish_handler.publish(
+            topic=topic, payload=payload, qos=qos, retain=retain
+        )
+
+    def add_topic_message_handler(self, topic: str, handler: Callable) -> int:
+        return self.message_handler.add_topic_message_handler(
+            topic=topic, handler=handler
+        )
+
+    def remove_topic_message_handler(self, topic: str, handler_id: int) -> None:
+        return self.message_handler.remove_topic_message_handler(
+            topic=topic, handler_id=handler_id
+        )
+
+    def add_topic_publish_handler(self, topic: str, handler: Callable) -> int:
+        return self.publish_handler.add_topic_publish_handler(
+            topic=topic, handler=handler
+        )
+
+    def remove_topic_publish_handler(self, topic: str, handler_id: int) -> None:
+        return self.publish_handler.remove_topic_publish_handler(
+            topic=topic, handler_id=handler_id
+        )
+
+    def add_topic_subscribe_handler(self, topic: str, handler: Callable) -> int:
+        return self.subscription_handler.add_topic_subscribe_handler(
+            topic=topic, handler=handler
+        )
+
+    def remove_topic_subscribe_handler(self, topic: str, handler_id: int) -> None:
+        return self.subscription_handler.remove_topic_subscribe_handler(
+            topic=topic, handler_id=handler_id
+        )
+
+    def add_topic_unsubscribe_handler(self, topic: str, handler: Callable) -> int:
+        return self.subscription_handler.add_topic_unsubscribe_handler(
+            topic=topic, handler=handler
+        )
+
+    def remove_topic_unsubscribe_handler(self, topic: str, handler_id: int) -> None:
+        return self.subscription_handler.remove_topic_unsubscribe_handler(
+            topic=topic, handler_id=handler_id
+        )
+
+    def add_connect_handler(self, handler: Callable) -> int:
+        return self.connection_handler.add_connect_handler(handler=handler)
+
+    def remove_connect_handler(self, handler_id: int) -> None:
+        return self.connection_handler.remove_connect_handler(handler_id=handler_id)
+
+    def add_disconnect_handler(self, handler: Callable) -> int:
+        return self.connection_handler.add_disconnect_handler(handler=handler)
+
+    def remove_disconnect_handler(self, handler_id: int) -> None:
+        return self.connection_handler.remove_disconnect_handler(handler_id=handler_id)
+
+    def run(self) -> None:
+        self.connection_handler.connect()
+        self.subscription_handler.enact_subscriptions()
+        self.client.loop_forever()
+
+    def add_topic_blueprint(self, blueprint: TopicBlueprint, topic_prefix: str = ""):
+        for topic in blueprint.topic_action_handlers:
+            for handler in blueprint.topic_action_handlers[topic].get("subscribe", []):
+                self.add_topic_subscribe_handler(topic=f"{topic_prefix}{topic}", handler=handler)
+            for handler in blueprint.topic_action_handlers[topic].get("unsubscribe", []):
+                self.add_topic_unsubscribe_handler(topic=f"{topic_prefix}{topic}", handler=handler)
+            for handler in blueprint.topic_action_handlers[topic].get("publish", []):
+                self.add_topic_publish_handler(topic=f"{topic_prefix}{topic}", handler=handler)
+            for handler in blueprint.topic_action_handlers[topic].get("message", []):
+                self.add_topic_message_handler(topic=f"{topic_prefix}{topic}", handler=handler)
+
+            self.subscribe(topic=f"{topic_prefix}{topic}")
+
