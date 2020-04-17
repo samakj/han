9dbfbc9cbbd3547665c59ea6db1332a06eb2ad49
Sam Jones <samakj@live.co.uk>
2020-04-17 09:30:54 +0100

    Add node controller docker image

---
+++ controllers/node/client.py
@@ -0,0 +1,16 @@
+import os
+
+from han_mqtt import HanMqttClient
+
+
+def create_mqtt_client() -> HanMqttClient:
+    client = HanMqttClient(
+        client_id="node_controller",
+        host=os.environ["BROKER_HOST"],
+        port=int(os.environ["BROKER_PORT"]),
+    )
+
+    return client
+
+
+client = create_mqtt_client()
