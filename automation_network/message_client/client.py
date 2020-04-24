b1323ac42a129f41d0971bc96a7e4a797931d46d
Sam Jones <samakj@live.co.uk>
2020-04-24 20:57:33 +0100

    Enable MQTT logger

--- automation_network/message_client/client.py
+++ automation_network/message_client/client.py
@@ -1,9 +1,13 @@
+import logging
 import os
 
 from han_mqtt import HanMqttClient
 
 from topics.report import REPORT_TOPIC_BLUEPRINT
 from topics.meta import META_TOPIC_BLUEPRINT
+
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 def create_mqtt_client() -> HanMqttClient:
@@ -12,6 +16,8 @@ def create_mqtt_client() -> HanMqttClient:
         host=os.environ["BROKER_HOST"],
         port=int(os.environ["BROKER_PORT"]),
     )
+
+    client.enable_logger(LOG)
 
     client.add_topic_blueprint(REPORT_TOPIC_BLUEPRINT, topic_prefix="/v0")
     client.add_topic_blueprint(META_TOPIC_BLUEPRINT, topic_prefix="/v0")
