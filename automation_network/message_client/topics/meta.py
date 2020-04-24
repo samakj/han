fd57689380ce12f2e4ada08e7222fc8bb2d5d4ad
Sam Jones <samakj@live.co.uk>
2020-04-24 20:26:15 +0100

    Use json load in init meta request

--- automation_network/message_client/topics/meta.py
+++ automation_network/message_client/topics/meta.py
@@ -4,6 +4,7 @@ from time import time
 from paho.mqtt.client import MQTTMessage
 from han_mqtt import HanMqttClient, MqttUserData
 from han_mqtt.topics.TopicBlueprint import TopicBlueprint
+from han_mqtt.topics.json_payload import json_payload
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(message)s")
@@ -12,8 +13,9 @@ META_TOPIC_BLUEPRINT = TopicBlueprint()
 
 
 @META_TOPIC_BLUEPRINT.topic("/meta", actions=["message"])
+@json_payload()
 def edge_init_meta(message: MQTTMessage, client: HanMqttClient, user_data: MqttUserData) -> None:
-    if str(message.payload.decode("utf-8")) == "init":
+    if message.payload.get("command") == "INIT_META":
         name = user_data.client_id or user_data.username if user_data else None
         LOG.info(f"{name or '< unknown >'} requested init meta data.")
         client.publish("/v0/meta", {"timestamp": int(time())})
