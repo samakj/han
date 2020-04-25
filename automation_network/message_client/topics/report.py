67227498cd733eb672cafd2cb72b04d3eec5ede7
Sam Jones <samakj@live.co.uk>
2020-04-25 13:04:30 +0100

    Stop checking client id on /report/attach'
     - The user_data sent to the call back is the clients not the
       message sender
     - Will have to think of new identification method

--- automation_network/message_client/topics/report.py
+++ automation_network/message_client/topics/report.py
@@ -5,7 +5,6 @@ from paho.mqtt.client import MQTTMessage
 from han_mqtt.models.UserData import MqttUserData
 from han_mqtt.topics.TopicBlueprint import TopicBlueprint
 from han_mqtt.topics.decorators.decode_message import decode
-from han_mqtt.security.confirm_client_id import confirm_client_id
 
 from secrets import SECRET_KEY
 
@@ -17,7 +16,6 @@ REPORT_TOPIC_BLUEPRINT = TopicBlueprint()
 
 @REPORT_TOPIC_BLUEPRINT.topic("/report/attach", actions=["message"])
 @decode(key=SECRET_KEY)
-@confirm_client_id()
 def attach(message: MQTTMessage, user_data: MqttUserData, **_):
     LOG.info(
         f"{MqttUserData.client_id if user_data else None} sent this to /report/attach:\n"
