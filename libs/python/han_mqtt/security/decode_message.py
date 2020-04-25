7e3d724f9cfa44ea868dbea76a6688aa27b9c6f0
Sam Jones <samakj@live.co.uk>
2020-04-25 12:09:46 +0100

    Extend decode decorator
     - All functionality can be toggled to match use case

--- libs/python/han_mqtt/security/decode_message.py
+++ libs/python/han_mqtt/security/decode_message.py
@@ -10,7 +10,14 @@ LOG = logging.getLogger(__name__)
 DEFAULT_EXPIRY_TIME = 1
 
 
-def decode(key: str, payload_expiry: int = DEFAULT_EXPIRY_TIME) -> Callable:
+def decode(
+    key: str,
+    payload_expiry: int = DEFAULT_EXPIRY_TIME,
+    confirm_timestamp: bool = True,
+    confirm_id: bool = True,
+    log_on_error: bool = True,
+    call_on_error: bool = False,
+) -> Callable:
     def decorator(func: Callable) -> Callable:
         def wrapper(client: MQTTClient, user_data: MqttUserData, message: MQTTMessage) -> Optional[Callable]:
             if message.payload:
@@ -25,18 +32,30 @@ def decode(key: str, payload_expiry: int = DEFAULT_EXPIRY_TIME) -> Callable:
                     )
                     return
 
-                if datetime.utcnow().timestamp() - decoded_payload.get("timestamp", 0) > payload_expiry:
-                    LOG.error(
-                        f"{datetime.utcnow()}: Expired payload sent to '{message.topic}'. "
-                        f"client_id: {client_id}."
-                    )
-                    return
-                if decoded_payload.get("node_id", None) != user_data.client_id:
-                    LOG.error(
-                        f"{datetime.utcnow()}: Invalid payload, bad id, sent to '{message.topic}'. "
-                        f"client_id: {client_id}."
-                    )
-                    return
+                if (
+                    confirm_timestamp and
+                    datetime.utcnow().timestamp() - decoded_payload.get("timestamp", 0) > payload_expiry
+                ):
+                    if log_on_error:
+                        LOG.error(
+                            f"{datetime.utcnow()}: Expired payload sent to '{message.topic}'. "
+                            f"client_id: {client_id}."
+                        )
+
+                    if not call_on_error:
+                        return
+                if (
+                    confirm_id and
+                    decoded_payload.get("node_id", None) != user_data.client_id
+                ):
+                    if log_on_error:
+                        LOG.error(
+                            f"{datetime.utcnow()}: Invalid payload, bad id, sent to '{message.topic}'. "
+                            f"client_id: {client_id}."
+                        )
+
+                    if not call_on_error:
+                        return
 
                 message.payload = decoded_payload
             return func(client=client, user_data=user_data, message=message)
