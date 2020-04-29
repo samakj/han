60e64d3e6e734a2322de844df1bb1c78abcfd02d
Sam Jones <samakj@live.co.uk>
2020-04-29 20:37:36 +0100

    Add sketch to send readings to mqtt server

--- automation_network/devices/build.py
+++ automation_network/devices/build.py
@@ -5,6 +5,7 @@ from os import listdir
 
 
 def build(path: str) -> None:
+    print(f"Building {path}...")
     build_dir = f"./builds/{path.strip('/')}"
 
     with open(f"{build_dir}/build.json") as file:
@@ -15,8 +16,10 @@ def build(path: str) -> None:
     with open(f"{sketch_dir}/config.json") as file:
         sketch_config = json.load(file)
 
+    print(f"Copying sketch file")
     copyfile(f"{sketch_dir}/sketch.ino", f"{build_dir}/{build_dir.split('/')[-1]}.ino")
 
+    print(f"Copying sketch dir files")
     for file in listdir(sketch_dir):
         if file not in {"sketch.ino", "config.json"}:
             copyfile(f"{sketch_dir}/{file}", f"{build_dir}/{file}")
@@ -28,12 +31,16 @@ def build(path: str) -> None:
     if sketch_config.get("configs", None) is not None:
         for config_path in sketch_config["configs"]:
             with open(f"./config/{config_path.strip('/')}") as file:
+                print(f"Adding sketch config file: {config_path}")
                 variables.update(json.load(file))
     if sketch_config.get("variables", None) is not None:
-        variables.update(build_config["variables"])
+        print(f"Adding sketch config variables")
+        variables.update(sketch_config["variables"])
     if build_config.get("variables", None) is not None:
+        print(f"Adding build sketch config variables")
         variables.update(build_config["variables"])
 
+    print(f"Writing config.h")
     with open(f"{build_dir}/config.h", "w") as file:
         for name, value in variables.items():
             if isinstance(value, str):
