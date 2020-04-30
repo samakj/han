4180ed7a0ff5d4d9a601f3597cb6507cce8d119d
Sam Jones <samakj@live.co.uk>
2020-04-30 10:45:27 +0100

    Remove timestamp check from report enpoint
     - Waiting for rest api to implement this

--- automation_network/message_client/topics/report.py
+++ automation_network/message_client/topics/report.py
@@ -3,7 +3,6 @@ import logging
 from paho.mqtt.client import MQTTMessage
 from han_mqtt.topics.TopicBlueprint import TopicBlueprint
 from han_mqtt.topics.decorators.decode_message import decode
-from han_mqtt.security.confirm_timestamp import confirm_timestamp
 
 from secrets import SECRET_KEY
 
@@ -15,6 +14,5 @@ REPORT_TOPIC_BLUEPRINT = TopicBlueprint()
 
 @REPORT_TOPIC_BLUEPRINT.topic("/report/#", actions=["message"])
 @decode(key=SECRET_KEY)
-@confirm_timestamp()
 def handle_report(message: MQTTMessage, **_) -> None:
     LOG.info(f"{message.payload['_id']}'s reported to '{message.topic}': {message.payload}.")
