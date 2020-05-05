b3c217ca8ad766869c3129db361ee10086ce3acd
Sam Jones <samakj@live.co.uk>
2020-05-05 20:52:17 +0100

    Add routes to the reports app

--- api/reports/app/app.py
+++ api/reports/app/app.py
@@ -5,6 +5,11 @@ from han_flask import HanFlask
 from han_sqlalchemy import create_database
 
 from handlers.report_creation_handler import ReportCreationHandler
+from routes.v0.humidity_value_routes import HUMIDITY_VALUES_V0_BLUEPRINT
+from routes.v0.motion_value_routes import MOTION_VALUES_V0_BLUEPRINT
+from routes.v0.report_metric_routes import REPORT_METRICS_V0_BLUEPRINT
+from routes.v0.report_routes import REPORTS_V0_BLUEPRINT
+from routes.v0.temperature_value_routes import TEMPERATURE_VALUES_V0_BLUEPRINT
 from stores.report_metric_store import ReportMetricStore
 from stores.temperature_value_store import TemperatureValueStore
 from stores.humidity_value_store import HumidityValueStore
@@ -46,6 +51,12 @@ def create_app() -> HanFlask:
         motion_value_store=app.motion_value_store,
         report_store=app.report_store,
     )
+
+    app.register_blueprint(HUMIDITY_VALUES_V0_BLUEPRINT, url_prefix="/v0")
+    app.register_blueprint(MOTION_VALUES_V0_BLUEPRINT, url_prefix="/v0")
+    app.register_blueprint(REPORT_METRICS_V0_BLUEPRINT, url_prefix="/v0")
+    app.register_blueprint(REPORTS_V0_BLUEPRINT, url_prefix="/v0")
+    app.register_blueprint(TEMPERATURE_VALUES_V0_BLUEPRINT, url_prefix="/v0")
 
     return app
 
