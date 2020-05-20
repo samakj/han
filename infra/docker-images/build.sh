35815c29e92516e9aa758e9cda4435181b15ffec
Sam Jones <samakj@live.co.uk>
2020-05-20 12:21:51 +0100

    Add mqqt docker image with TLS, auth and acl

--- infra/docker-images/build.sh
+++ infra/docker-images/build.sh
@@ -4,11 +4,13 @@ cd "${PWD/\/HAN\/*//HAN}"
 
 
 docker rmi han-mqtt:latest
+docker rmi han-secure-mqtt:latest
 docker rmi han-postgres:latest
 docker rmi han-python:latest
 docker rmi han-redis:latest
 
 docker build ./infra/docker-images/mqtt1.6/ --tag han-mqtt:latest
+docker build ./infra/docker-images/secure-mqtt1.6/ --tag han-secure-mqtt:latest
 docker build ./infra/docker-images/postgres12.0/ --tag han-postgres:latest
 docker build . -f ./infra/docker-images/python3.7/Dockerfile --tag han-python:latest
 docker build ./infra/docker-images/redis5.0/ --tag han-redis:latest
