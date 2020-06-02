2a08db70d8fd7c2489a0d1d3d1b0e768f03a6310
Sam Jones <samakj@live.co.uk>
2020-06-02 11:03:16 +0100

    Add todos to the api services for keepting track

---
+++ api/broker/app/__init__.py
@@ -0,0 +1,4 @@
+# TODO: Create roles for different parts of system
+#   - Broker only needs access to verify routes
+#   - Build tool only needs update/create
+#   - Other users only have access to get
---
+++ api/devices/app/__init__.py
@@ -0,0 +1,2 @@
+# TODO: Rename report metric table to just metric (& device type report metric) table.
+#   - This will allow for them to be used in future for commands too.
