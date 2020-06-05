75333f5270692b42bd160a5ecf2c9fdb19fe8357
Sam Jones <samakj@live.co.uk>
2020-06-05 09:43:40 +0100

    Remove leading slash in topic name

--- network/client/client.py
+++ network/client/client.py
@@ -22,7 +22,7 @@ def create_mqtt_client() -> MidgeMqttClient:
     )
 
     mqtt_client.enable_logger(LOG)
-    mqtt_client.add_topic_blueprint(REPORT_TOPIC_BLUEPRINT, topic_prefix="/v0")
+    mqtt_client.add_topic_blueprint(REPORT_TOPIC_BLUEPRINT, topic_prefix="v0")
 
     return mqtt_client
 
