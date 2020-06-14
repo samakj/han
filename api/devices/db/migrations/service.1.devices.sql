1e09990122447d83dd4fe05dbead40a8c856783b
Sam Jones <samakj@live.co.uk>
2020-06-14 17:43:45 +0100

    Add lastest_ping to devices table

--- api/devices/db/migrations/service.1.devices.sql
+++ api/devices/db/migrations/service.1.devices.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS device_types
 (
     device_type_id SERIAL PRIMARY KEY,
-    name           TEXT    NOT NULL UNIQUE,
+    name           TEXT NOT NULL UNIQUE,
     report_period  INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS devices
 (
     device_id      TEXT PRIMARY KEY,
-    device_type_id INTEGER NOT NULL REFERENCES device_types (device_type_id)
+    device_type_id INTEGER NOT NULL REFERENCES device_types (device_type_id),
+    latest_ping    TIMESTAMP
 );
